ce963dd110932aca5a667aeeae808423678ca081
龍一 <ryuichi.comet2215@gmail.com>
2019-06-13 00:07:41 +0900

    sample commit

--- sample.py
+++ sample.py
@@ -17,6 +17,7 @@ TEXT_CHANNEL = "545291639141171210" # テキストチャットのチャンネル
 # https://api.matsurihi.me/docs/
 RequestURL = "https://api.matsurihi.me/mltd/v1/"
 RequestEventsURL = RequestURL + "events/"
+GetRankNumber = "1,2,3,100,101,2500,5000"
 """
 APIVersion = "version/latest"
 EventType = "/rankings/logs/eventPoint/"
@@ -49,32 +50,65 @@ async def greeting_gm():
     """
     # 起動後にはイベント情報を取得する
     # イベント情報取得
-    event_info = GetEventInfo()
-    if event_info == "":
+    eventInfo = GetEventInfo()
+
+    if eventInfo == "":
         msg = "現在開催中のイベントはありません"
     else:
-        event_type = event_info['type']
-        event_id = event_info['id']
+        eventType = eventInfo['type']
+        eventId = eventInfo['id']
         # イベント情報メッセージ生成
-        msg = GetEventInfoMsg(event_info)
+        msg = GetEventInfoMsg(eventInfo)
 
     await client.send_message(text_chat, msg)
-
-
 
     cnt = 0
     lastDate = ""
     while cnt == 0:
         nowTime = datetime.datetime.now()
-        event_type = -1
+        eventType = -1
+
+        # ToDo : 00:00ならイベント情報を再取得する
+        if nowTime.hour == "0" and nowTime.minute == "0" :
+            eventInfo = GetEventInfo()
+            if eventInfo == "":
+                msg = "現在開催中のイベントはありません"
+            else:
+                eventType = eventInfo['type']
+                eventId = eventInfo['id']
+                # イベント情報メッセージ生成
+                msg = GetEventInfoMsg(eventInfo)
+
 
         # 次の取得時刻の計算
         await client.send_message(text_chat, nowTime)
 
+        # 1.Typeを確認し、シアターもしくはツアーでならば実行
+        # ミリコレ、ワーキングなら実行しない
+        #if eventType == 3 or eventType == 4:
+        if eventType == 3 or eventType == 4:
 
+            # 2.前回の更新から40分以上経過 or 初取得であれば実行
+            if lastDate == "" :
 
-        # テストとして1分ごとにループする
-        interval = 10
+                # 取得処理開始
+                # ポイントランキング取得
+                rankingInfo = GetEventPointRanking(eventId)
+                # ランキング更新判定
+                if ChkUpdateRanking(rankingInfo, lastDate):
+                    msg = GetEventPointRankingMsg(rankingInfo)
+                    await client.send_message(text_chat, msg)
+
+                    # 取得時刻の追記
+                    dataCnt = len(rankingInfo[0]['data']) - 1
+                    lastDate = TimeConversion(rankingInfo[0]['data'][dataCnt]['summaryTime'])
+                    print(lastDate)
+                # if ChkUpdateRanking(rankingInfo, lastDate):
+            # if lastDate == "" :
+        # if eventType == 3 or eventType == 4:
+
+        # 60秒ごとにループする
+        interval = 60
         time.sleep(interval)
 
     # while true :
@@ -90,8 +124,12 @@ async def greeting_gm():
 ###################
 def GetEventInfo():
    # ex) https://api.matsurihi.me/mltd/v1/events?type=theater&at=2019-02-09
-   AT = "at=" + datetime.datetime.now().strftime("%Y-%m-%d")
-   reqeustAction = urllib.request.Request(RequestEventsURL + "?" + AT)
+   # 注意：時刻指定の場合、イベント開始日にはAPIが対応していない模様
+   #       なので、イベント情報をすべて引っ張ってきて最後のidの情報の終了日が今の日付より前ならイベントありと判断する
+   orderBy = "beginTime" + datetime.datetime.now().strftime("%Y-%m-%d")
+   requestUrl = RequestEventsURL + "?" + orderBy
+   print(requestUrl)
+   reqeustAction = urllib.request.Request(requestUrl)
 
    with urllib.request.urlopen(reqeustAction) as responceData:
        body = responceData.read()
@@ -101,9 +139,22 @@ def GetEventInfo():
        if len(jsonDict) == 0:
            eventInfoJson = ""
        else :
-            eventInfoJson = jsonDict[0]
+            lastEventNumber = len(jsonDict) - 1
+            print(len(jsonDict))
+            eventInfoJson = jsonDict[lastEventNumber]
+            tmpArray = TimeConversion(eventInfoJson['schedule']['endDate'])
+
+            # フォーマット変換
+            endDate = datetime.datetime.strptime(tmpArray[0], '%Y-%m-%d')
+            endTime = datetime.datetime.strptime(tmpArray[1], '%H:%M:%S')
+            nowTime = datetime.datetime.now()
+
+            # 現在の時刻を取得し比較する
+            if endDate.date() <= nowTime.date() and endTime.strftime("%H:%M:%S") < nowTime.strftime("%H:%M:%S") :
+                eventInfoJson = ""
 
    return eventInfoJson
+
 # def get_event_info():
 
 ###################
@@ -149,6 +200,28 @@ def GetEventInfoMsg(eventInfoJson):
     return msg
 #def GetEventInfoMsg(eventInfoJson):
 
+
+#########################
+# ランキング情報取得関数
+# 引数：時刻
+# 戻り値：APIから取得したポイントランキング情報
+#########################
+def GetEventPointRanking(getId):
+
+    # ex) https://api.matsurihi.me/mltd/v1/events/33/rankings/logs/eventPoint/1,2,3
+    requestUrl = RequestEventsURL + "/" + str(getId) + "/rankings/logs/eventPoint/" + GetRankNumber
+    req = urllib.request.Request(requestUrl)
+
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+
+        # 配列で取得される。1つ目の要素を取得
+        json_dict = json.loads(body)
+        #json_dict = json_dict[0]
+    return json_dict
+
+#def get_event_point_ranking(now_time):
+
 #########################
 # 時刻変換関数
 # 引数：date_time APIから取得したイベント時刻
@@ -162,9 +235,57 @@ def TimeConversion(date_time):
 
     return return_ary
 
+#########################
+# ポイントランキング更新判定関数
+# 引数：ランキング情報, 最終日時
+# 戻り値：boolean true 更新されている false 更新されていない
+#########################
+def ChkUpdateRanking(getRanking, lastTime):
 
+    dataCnt= len(getRanking[0]['data']) - 1
+    tmpArray = TimeConversion(getRanking[0]['data'][dataCnt]['summaryTime'])
+    updateChkFlg = False
 
+    if lastTime == "" :
+        updateChkFlg = True
+    elif tmpArray[0] != lastTime[0] or  tmpArray[1] != lastTime[1] :
+        updateChkFlg = True
 
+    return updateChkFlg
+
+#########################
+# ランキング情報整形関数
+# 引数：APIから取得したポイントランキング
+# 戻り値：メッセージ
+#########################
+def GetEventPointRankingMsg(getRanking):
+
+    data_cnt= len(getRanking[0]['data']) - 1
+    tmp_ary = TimeConversion(getRanking[0]['data'][data_cnt]['summaryTime'])
+
+    msg = ""
+    msg += "```" + "\n"
+    msg += "取得日時:" + tmp_ary[0]  +" " + tmp_ary[1] + "\n"
+
+    for get_rank_data in getRanking :
+        get_rank = get_rank_data['rank']
+        get_data = get_rank_data['data']
+
+        if len(get_data) > 2:
+            now_data = get_data[len(get_data) - 1]
+            old_data = get_data[len(get_data) - 2]
+            now_score = int(now_data['score'])
+            old_score = int(old_data['score'])
+            diff_score = now_score - old_score
+            msg += str(get_rank) + "位:  " + str(now_score) + "pt ( +" + str(diff_score) + "pt)"+ "\n"
+
+        #else :
+
+    # for get_rank_data in get_ranking :
+    msg += "```"+ "\n"
+    return msg
+
+#get_event_point_ranking_msg():
 
 ###############################
 # ボット起動
